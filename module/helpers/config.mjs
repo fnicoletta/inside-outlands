8fb3eca5516774106ece5b64d83a1f9a3f1da5c6
Franky Khoury <fnicoletta97@gmail.com>
2024-08-25 14:17:08 -0700

    initial

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const INSIDE_OUTLANDS = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+INSIDE_OUTLANDS.abilities = {
+  str: 'INSIDE_OUTLANDS.Ability.Str.long',
+  dex: 'INSIDE_OUTLANDS.Ability.Dex.long',
+  con: 'INSIDE_OUTLANDS.Ability.Con.long',
+  int: 'INSIDE_OUTLANDS.Ability.Int.long',
+  wis: 'INSIDE_OUTLANDS.Ability.Wis.long',
+  cha: 'INSIDE_OUTLANDS.Ability.Cha.long',
+};
+
+INSIDE_OUTLANDS.abilityAbbreviations = {
+  str: 'INSIDE_OUTLANDS.Ability.Str.abbr',
+  dex: 'INSIDE_OUTLANDS.Ability.Dex.abbr',
+  con: 'INSIDE_OUTLANDS.Ability.Con.abbr',
+  int: 'INSIDE_OUTLANDS.Ability.Int.abbr',
+  wis: 'INSIDE_OUTLANDS.Ability.Wis.abbr',
+  cha: 'INSIDE_OUTLANDS.Ability.Cha.abbr',
+};
